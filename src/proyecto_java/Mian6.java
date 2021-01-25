7bdcb8bff5a6c91c6c0a0f9ba1a7322a4ba9d6f6
Conchi <conchiballesteros2000@gmail.com>
2021-01-25 20:44:26 +0100

    SNAPSHOT5 BUENA

---
+++ src/proyecto_java/Mian6.java
@@ -0,0 +1,12 @@
+
+package proyecto_java;
+
+
+public class Mian6 {
+
+    public static void main(String[] args) {
+         int a1[] = {2,7,6,1,5};
+         int a2[] = {10,9,3,4,11,12,13,14,15};
+    }
+    
+}
