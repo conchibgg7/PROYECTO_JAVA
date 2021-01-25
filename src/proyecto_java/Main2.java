95843b7c7a0aeeb6045d5cd089d80b5627d91937
Conchi <conchiballesteros2000@gmail.com>
2021-01-25 19:37:15 +0100

    SNAPSHOT2

---
+++ src/proyecto_java/Main2.java
@@ -0,0 +1,13 @@
+
+package proyecto_java;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        char[] caracter = {'H', 'O', 'L', 'A'};
+        String cadena = String.valueOf(caracter);
+        System.out.println(cadena);
+    }
+}
+    
+
