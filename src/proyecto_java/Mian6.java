de98c139439d197fa4dd333d25a2fb2cd845c244
Conchi <conchiballesteros2000@gmail.com>
2021-01-25 20:48:45 +0100

    SNAPSHOT6

--- src/proyecto_java/Mian6.java
+++ src/proyecto_java/Mian6.java
@@ -5,8 +5,17 @@ package proyecto_java;
 public class Mian6 {
 
     public static void main(String[] args) {
-         int a1[] = {2,7,6,1,5};
-         int a2[] = {10,9,3,4,11,12,13,14,15};
+        int a1[] = {2, 7, 6, 1, 5};
+        int a2[] = {10, 9, 3, 4, 11, 12, 13, 14, 15};
+        System.arraycopy(a1, 0, a2, 0, 1);
+        System.out.print("array2 = ");
+        System.out.print(a2[4] + " ");
+        System.out.print(a2[5] + " ");
+        System.out.print(a2[6] + " ");
+        System.out.print(a2[7] + " ");
+        System.out.print(a2[8] + " ");
+        System.out.print(a2[9] + " ");
     }
-    
 }
+    
+
